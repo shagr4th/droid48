8a1d6753bcef62a5de68a217436a52599246c411
Denis Bernard <denis@personalized-software.ie>
2011-06-27 21:23:06 +0200

    Fixed timezone issue
    Timezone offset was not working.

--- jni/Android.mk
+++ jni/Android.mk
@@ -30,7 +30,7 @@ LOCAL_MODULE    := droid48
 
 LOCAL_LDLIBS := -L${SYSROOT}/usr/lib -llog
 
-LOCAL_SRC_FILES := config.h actions.c binio.c debugger.c device.c \
+LOCAL_SRC_FILES := actions.c binio.c debugger.c device.c \
               disasm.c emulate.c init.c \
               lcd.c main.c memory.c options.c register.c \
               resources.c romio.c rpl.c serial.c timer.c x48.c
